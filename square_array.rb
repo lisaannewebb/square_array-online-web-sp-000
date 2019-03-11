4e783a646ab93bf3b69c617003774c9c9d15bff0
Lisa Webb <lwebbnyc@outlook.com>
2019-03-11 17:12:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,8 @@
 def square_array(array)
-
  new_array = []
 
- array.each do |integer|
-  new_array << integer ** 2
+ array.each do |number|
+  new_array << number ** 2
  end
  return new_array
 end
